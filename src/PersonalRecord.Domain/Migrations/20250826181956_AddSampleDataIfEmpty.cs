4e07c485d93bbbf62e8063035e087147168dbbd1
Ramazan Yilmaz <r.yilmaz@outlook.com>
2025-10-17 22:22:33 +0200

    Check entries before deletion on migration down

--- src/PersonalRecord.Domain/Migrations/20250826181956_AddSampleDataIfEmpty.cs
+++ src/PersonalRecord.Domain/Migrations/20250826181956_AddSampleDataIfEmpty.cs
@@ -50,13 +50,20 @@ namespace PersonalRecord.Domain.Migrations
                 DELETE FROM MovementRecordItems 
                 WHERE MovementRecordID IN (
                     '7700db04-f8a1-4fc4-a54e-49cac742178d', 
-                    'ecb6eed0-7b63-4b14-ba50-d9dc7d43dc27');
-
+                    'ecb6eed0-7b63-4b14-ba50-d9dc7d43dc27')
+                AND EXISTS (SELECT 1 FROM MovementRecordItems WHERE MovementRecordID IN (
+                    '7700db04-f8a1-4fc4-a54e-49cac742178d', 
+                    'ecb6eed0-7b63-4b14-ba50-d9dc7d43dc27'));
+    
                 DELETE FROM MovementItems 
                 WHERE MovementID IN (
                     '80a917f2-299b-422d-a5fe-3a2842f21e29', 
                     '5e1808aa-6ed8-4dac-9bd2-4317d4f27376', 
-                    '8613aa4d-154a-4423-8ffa-63cfcaa063b2');
+                    '8613aa4d-154a-4423-8ffa-63cfcaa063b2')
+                AND EXISTS (SELECT 1 FROM MovementItems WHERE MovementID IN (
+                    '80a917f2-299b-422d-a5fe-3a2842f21e29', 
+                    '5e1808aa-6ed8-4dac-9bd2-4317d4f27376', 
+                    '8613aa4d-154a-4423-8ffa-63cfcaa063b2'));
             ");
         }
     }
