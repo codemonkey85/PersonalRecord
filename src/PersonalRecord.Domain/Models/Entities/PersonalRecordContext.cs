e3a0823c88f1dbd6cd30e7d47b2ee5196584447f
Ramazan Yilmaz <r.yilmaz@outlook.com>
2025-10-18 09:24:01 +0200

    Use seed data to add sample entries

--- src/PersonalRecord.Domain/Models/Entities/PersonalRecordContext.cs
+++ src/PersonalRecord.Domain/Models/Entities/PersonalRecordContext.cs
@@ -1,6 +1,7 @@
 ﻿namespace PersonalRecord.Domain.Models.Entities
 {
     using Microsoft.EntityFrameworkCore;
+    using PersonalRecord.Infrastructure.Constants;
 
     public class PersonalRecordContext : DbContext
     {
@@ -12,5 +13,44 @@
         public DbSet<Movement> MovementItems { get; set; }
 
         public DbSet<MovementRecord> MovementRecordItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSeeding((context, _) =>
+                {
+                    var any = context.Set<Movement>().Any();
+                    if (!any)
+                    {
+                        context.Set<Movement>().AddRange(GetSampleMovements());
+                        context.SaveChanges();
+                    }
+                })
+                .UseAsyncSeeding(async (context, _, cancellationToken) =>
+                {
+                    var any = await context.Set<Movement>().AnyAsync(cancellationToken);
+                    if (!any)
+                    {
+                        await context.Set<Movement>().AddRangeAsync(GetSampleMovements(), cancellationToken);
+                        await context.SaveChangesAsync(cancellationToken);
+                    }
+                });
+        }
+
+        private IEnumerable<Movement> GetSampleMovements()
+        {
+            var movements = new List<Movement>
+                {
+                    new() {
+                        MovementID = SampleConstants.SampleCleanId,
+                        MovName = SampleConstants.SAMPLE_CLEAN_NAME
+                    },
+                    new() {
+                        MovementID = SampleConstants.SampleDeadliftId,
+                        MovName = SampleConstants.SAMPLE_DEADLIFT_NAME
+                    }
+                };
+            return movements;
+        }
     }
 }
